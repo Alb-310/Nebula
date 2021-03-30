3f3ecd2428f62d46dfbba18a89f3aefc57e48eb9
Adrien HICAUBERT <adrien.hicaubert@epita.fr>
2021-03-30 12:23:34 +0200

    line_to

--- project-files/src/.h/drawing.h
+++ project-files/src/.h/drawing.h
@@ -4,5 +4,8 @@
 void set_pixel (gdImagePtr im, FILE *out, int x, int y, int r, int g, int b,
                     int a, int tks, int type, int zoom, char *path);
 
+void line_to (gdImagePtr im, FILE *out, int x1, int y1, int x2, int y2, 
+                int r, int g, int b, int a, int tks, int type, int zoom, char *path);
+
 #endif;
 
