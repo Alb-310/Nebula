f9bec1689537ea599fc24522ed6a0d3a4be18f3e
Phi-hung HOANG <phi-hung.hoang@epita.fr>
2021-05-03 21:14:04 +0200

    motif insertion in git

--- project-files/src/.h/func_divers.h
+++ project-files/src/.h/func_divers.h
@@ -1,14 +1,16 @@
 #ifndef FUNC_DIVERS_H
 #define FUNC_DIVERS_H
 
-void Resize(gdImagePtr im, FILE *out, char *path, int width, int height);
+void Resize(char *filename, int width, int height);
 
-void Crop(gdImagePtr im, FILE *out, char *path,int x,int y, int width,int height);
+void Crop(char *filename,int x,int y, int width,int height);
 
-void Rotate(gdImagePtr im, FILE *out, char *path,int angle);
+void Rotate(char *filename,int angle);
 
-void Add_text(gdImagePtr im, FILE *out, char *path,char *font,int x,int y,int width,int height,int color,double size,double angle,char* text);
+void Add_text(char *filename,char *font,int x,int y,int width,int height,int color,double size,double angle,char* text);
 
 void picture_insertion(char *filename_destination,char *filename_source,int dst_x,int dst_y,float src_dim_percent);
+
+void add_motif(char *filename_destination,char *motif,int dst_x,int dst_y, float src_dim_percent);
 #endif
 
